067bd6bda8e447b80820219e4619a35333536e4a
Andrzej Wisniewski <andrzej@loom.com>
2023-10-27 14:57:02 +0200

    small refactoring

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.21
 require github.com/rs/zerolog v1.31.0
 
 require (
+	github.com/aws/smithy-go v1.15.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/kr/pretty v0.3.1 // indirect
@@ -16,8 +17,10 @@ require (
 
 require (
 	github.com/aws/aws-sdk-go v1.46.4
+	github.com/aws/aws-sdk-go-v2 v1.21.2
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.8.4
 	golang.org/x/sync v0.4.0
 	golang.org/x/sys v0.13.0 // indirect
--- pkg/aws/regions.go
+++ pkg/aws/regions.go
@@ -3,13 +3,10 @@ package aws
 import (
 	"context"
 
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/rs/zerolog/log"
 )
-
-func boolPointer(b bool) *bool {
-	return &b
-}
 
 func GetRegionsToCheck(regionsVar []string, awsEc2 Ec2, ctx context.Context) ([]string, error) {
 	var regions []string
@@ -18,7 +15,7 @@ func GetRegionsToCheck(regionsVar []string, awsEc2 Ec2, ctx context.Context) ([]
 	if len(regionsVar) == 0 {
 		logWithContext.Debug().Msg("looking for regions")
 
-		input := &ec2.DescribeRegionsInput{AllRegions: boolPointer(true)}
+		input := &ec2.DescribeRegionsInput{AllRegions: aws.Bool(true)}
 		result, err := awsEc2.DescribeRegions(input)
 		if err != nil {
 			return nil, err
